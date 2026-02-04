44cd2e8decc6a7be4a0f4843f44a4f136a20d6a9
juul <jdavelaar0@gmail.com>
2026-02-04 15:44:21 +0100

    #16
    JS toegevoegd

---
+++ Asciiconv.js
@@ -0,0 +1,9 @@
+let convert = document.querySelector('.convert-button')
+
+convert.addEventListener('click', function() {
+
+document.querySelector('.notation').classList.toggle('notation-visible')
+
+
+console.log('script')
+})
